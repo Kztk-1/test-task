0ce65b7a7d2302cf482c8e0f35af0ec5762a95d3
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-05-24 11:50:10 +0300

    feat: к модели User добавлена @AllArgsConstructor

--- test-task/src/main/java/com/kztk/test_task/model/User.java
+++ test-task/src/main/java/com/kztk/test_task/model/User.java
@@ -2,12 +2,14 @@ package com.kztk.test_task.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private Long id;
